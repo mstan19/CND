2138a814d63856622a3e28fa9abd67e1acd40cc2
melissa <melissastan91@gmail.com>
2023-10-31 10:22:13 -0400

    testing

--- src/main/java/com/javaproject/CND/controller/DockerMessageController.java
+++ src/main/java/com/javaproject/CND/controller/DockerMessageController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerMessageController {
     @GetMapping("/")
     public String getMessage() {
-        return "Hello from cnd!";
+        return "Hello from CND!";
     }
 }
